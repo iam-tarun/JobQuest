7f11e4ac24fc38af91d3410ac82ebcedb44333be
Tarunteja Obbina <tarun@Mac.lan>
2024-12-16 14:45:21 -0600

    add applications screen

--- job-quest-client/src/app/Screens/applications/applications.component.ts
+++ job-quest-client/src/app/Screens/applications/applications.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApplicationService } from '../../services/application.service';
+import { Application } from '../../models/application';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-applications',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './applications.component.html',
   styleUrl: './applications.component.css'
 })
-export class ApplicationsComponent {
+export class ApplicationsComponent implements OnInit {
 
+  constructor(private applicationService: ApplicationService) {}
+
+  applications: Array<Application> = [];
+
+  ngOnInit(): void {
+      this.applicationService.getApplications().subscribe({
+        next: (response) => {
+          this.applications = response;
+        },
+        error: (e) => {
+          console.log(e);
+        }
+      })
+  }
 }
---
+++ job-quest-client/src/app/enums/application-status.ts
@@ -0,0 +1,7 @@
+export enum ApplicationStatus {
+  NOT_APPLIED = "Not Applied",
+  APPLIED = "Applied",
+  IN_PROGRESS = "In Process",
+  INTERVIEWS = "Interviews",
+  REJECTED = "Rejected"
+}
