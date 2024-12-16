cd4326c7670551df22a83af88fe8da6ccdd61292
Tarunteja Obbina <tarun@Mac.lan>
2024-12-16 10:25:11 -0600

    update interceptor to validate token

--- job-quest-client/src/app/interceptors/auth.interceptor.ts
+++ job-quest-client/src/app/interceptors/auth.interceptor.ts
@@ -1,17 +1,23 @@
 import { inject } from '@angular/core';
 import { HttpInterceptorFn } from '@angular/common/http';
+import { isTokenValid } from '../utils/validate_token';
+import { Router } from '@angular/router';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('jwtToken');
 
   const isPublicEndpoint = req.url.includes('/login') || req.url.includes('/register');
 
-  if (token && !isPublicEndpoint) {
+  if (token && !isPublicEndpoint && isTokenValid(token)) {
     // Clone the request and add the Authorization header
     const modifiedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`) 
     });
     return next(modifiedReq);
+  }
+
+  if (token && !isTokenValid(token) && !isPublicEndpoint) {
+    localStorage.removeItem('jwtToken');
   }
   // If no token, pass the original request
   return next(req);
