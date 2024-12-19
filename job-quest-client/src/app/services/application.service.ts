265f51e4c893ff1b06ca119e97ba75feeb5f778b
Tarunteja Obbina <tarun@Mac.lan>
2024-12-19 15:19:14 -0600

    add env setup

--- job-quest-client/src/app/services/application.service.ts
+++ job-quest-client/src/app/services/application.service.ts
@@ -2,33 +2,34 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Application } from '../models/application';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApplicationService {
 
-  private baseUrl = 'http://localhost:8080/api/application';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   submitApplication(data: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}`, data);
+    return this.http.post(`${this.baseUrl}/application`, data);
   }
 
   getApplications(): Observable<Application[]> {
-    return this.http.get<any[]>(`${this.baseUrl}`);
+    return this.http.get<any[]>(`${this.baseUrl}/application`);
   }
 
   getApplicationStats(): Observable<{"total": number, "rejected": number}> {
-    return this.http.get<{"total": number, "rejected": number}>(`${this.baseUrl}/stats`);
+    return this.http.get<{"total": number, "rejected": number}>(`${this.baseUrl}/application/stats`);
   }
 
   updateApplication(application: Application): Observable<Application> {
-    return this.http.put<Application>(this.baseUrl, application);
+    return this.http.put<Application>(this.baseUrl+'/application', application);
   }
 
   deleteApplication(id: number): Observable<void>{
-    return this.http.delete<void>(this.baseUrl+'/'+id);
+    return this.http.delete<void>(this.baseUrl+'/application/'+id);
   }
 }
