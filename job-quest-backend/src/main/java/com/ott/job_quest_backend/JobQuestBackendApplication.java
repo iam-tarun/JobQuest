6c34b6bb8e444900f9068e66f1a43d22f6a762bd
Tarunteja Obbina <tarun@Mac.lan>
2025-02-04 01:18:11 -0600

    enable serializing page data to json

--- job-quest-backend/src/main/java/com/ott/job_quest_backend/JobQuestBackendApplication.java
+++ job-quest-backend/src/main/java/com/ott/job_quest_backend/JobQuestBackendApplication.java
@@ -3,8 +3,12 @@ package com.ott.job_quest_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class JobQuestBackendApplication {
 
 	public static void main(String[] args) {
