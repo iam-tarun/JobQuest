02e7ba6bbc36144759bea613ff9a591474d7a32d
Tarunteja Obbina <tarun@Mac.lan>
2025-02-04 01:00:39 -0600

    add pagination feature for fetching applications

--- job-quest-backend/src/main/java/com/ott/job_quest_backend/repo/ApplicationRepo.java
+++ job-quest-backend/src/main/java/com/ott/job_quest_backend/repo/ApplicationRepo.java
@@ -2,6 +2,8 @@ package com.ott.job_quest_backend.repo;
 
 import com.ott.job_quest_backend.model.Application;
 import com.ott.job_quest_backend.model.ApplicationStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +11,9 @@ import java.util.List;
 
 @Repository
 public interface ApplicationRepo extends JpaRepository<Application, Integer> {
-    List<Application> findByUserId(int user);
+    Page<Application> findByUserId(int user, Pageable pageable);
+
+    List<Application> findByUser(int user);
 
     List<Application> findByUserIdAndStatus(int user, ApplicationStatus status);
 }
